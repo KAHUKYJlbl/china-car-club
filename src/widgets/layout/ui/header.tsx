c1b73fd2d833621c27f060f884c8fd6d634673a8
Iurii Pototskii <iuriipototskii@gmail.com>
2023-11-26 20:20:09 +0100

    feat: layout and header

--- src/widgets/layout/ui/header.tsx
+++ src/widgets/layout/ui/header.tsx
@@ -1,22 +1,28 @@
+import cn from 'classnames';
+
+import classes from './header.module.sass';
+
 export const Header = (): JSX.Element => {
   return (
-    <div>
-      <div>
+    <div className={classes.wrapper}>
+      <div className={classes.logo}>
         <svg width="214" height="26" aria-hidden="true">
           <use xlinkHref="#logo" />
         </svg>
       </div>
 
-      <div>
-        <div>
-          Экспертная компания
+      <div className={classes.navWrapper}>
+        <div className={classes.navHeader}>
+          <span>Экспертная компания,</span><br/>где есть офисы в Китае, России и в<br/>Киргизии
         </div>
 
-        <nav>
-          <div>Журнал</div>
-          <div>Контакты</div>
-          <div>О компании</div>
-          <div>8-800-2222-878</div>
+        <nav className={classes.nav}>
+          <div className={classes.navItem}>Журнал</div>
+          <div className={classes.navItem}>Контакты</div>
+          <div className={classes.navItem}>О компании</div>
+          <div className={cn(classes.navItem, classes.navPhone)}>
+            <a href='tel:+78002222878'>8-800-2222-878</a>
+          </div>
         </nav>
       </div>
     </div>
