4c2f4a5ad5be52d2a34ab1ce990e1567f1805a1a
Iurii Pototskii <iuriipototskii@gmail.com>
2023-11-29 21:05:34 +0100

    fat: gallery

--- src/shared/ui/gallery/ui/gallery-pagination.tsx
+++ src/shared/ui/gallery/ui/gallery-pagination.tsx
@@ -1,15 +1,23 @@
+import cn from 'classnames';
+
 import classes from './gallery-pagination.module.sass';
 
 type GalleryPaginationProps = {
   count: number;
+  current: number;
+  onClick: React.Dispatch<React.SetStateAction<number>>;
 }
 
-export const GalleryPagination = ({ count }: GalleryPaginationProps): JSX.Element => {
+export const GalleryPagination = ({ count, current, onClick }: GalleryPaginationProps): JSX.Element => {
   return (
     <div className={classes.wrapper}>
       {
         Array(count).fill('').map((value, index) => (
-          <div className={classes.bar} key={value + index} />
+          <div
+            className={ cn(classes.bar, {[classes.current]: index === current}) }
+            onClick={() => onClick(index)}
+            key={ value + index }
+          />
         ))
       }
     </div>
--- src/shared/ui/gallery/ui/gallery.tsx
+++ src/shared/ui/gallery/ui/gallery.tsx
@@ -1,14 +1,47 @@
+import { useState } from 'react';
+import { useSwipeable } from 'react-swipeable';
+
 import { GalleryPagination } from './gallery-pagination';
 import classes from './gallery.module.sass';
 
+const IMAGES_COUNT = 6;
+const IMAGES_NAMES = Array<string>(IMAGES_COUNT).fill('').map(( e, i ) => e + i)
+
 export const Gallery = (): JSX.Element => {
+  const [ currentImage, setCurrentImage ] = useState(0);
+  const handlers = useSwipeable({
+    onSwipedLeft: handleNext,
+    onSwipedRight: handlePrev,
+  });
+
+  function handleNext () {
+    setCurrentImage((current) =>
+      current + 1 === IMAGES_NAMES.length
+      ? 0
+      : current + 1
+    )
+  };
+
+  function handlePrev () {
+    setCurrentImage((current) =>
+      current === 0
+      ? IMAGES_NAMES.length - 1
+      : current - 1
+    )
+  };
+
   return (
     <div
       className={classes.wrapper}
-      style={{backgroundImage: "url('./images/gallery/car.jpg')"}}
+      style={{backgroundImage: `url('./images/gallery/car-${currentImage + 1}.jpg')`}}
+      {...handlers}
     >
       <div>
-        <GalleryPagination count={6} />
+        <GalleryPagination
+          count={IMAGES_COUNT}
+          current={currentImage}
+          onClick={setCurrentImage}
+        />
 
         <p>
           LiXiang L9
@@ -17,13 +50,13 @@ export const Gallery = (): JSX.Element => {
 
       <div className={classes.controls}>
         <div className={classes.arrows}>
-          <button>
+          <button onClick={handlePrev}>
             <svg width="9" height="8" aria-hidden="true">
               <use xlinkHref="#arrow-left" />
             </svg>
           </button>
 
-          <button>
+          <button onClick={handleNext}>
             <svg width="9" height="8" aria-hidden="true">
               <use xlinkHref="#arrow-right" />
             </svg>
