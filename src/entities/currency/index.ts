0f74e4b1bc7e02eb2a6ba699f4ab693b8557b084
Iurii Pototskii <iuriipototskii@gmail.com>
2024-01-25 22:41:15 +0100

    feat: colors

--- src/entities/currency/index.ts
+++ src/entities/currency/index.ts
@@ -2,3 +2,4 @@ export { currencySlice } from './model/currency-slice';
 export { Currency } from './ui/currency';
 export type { CurrencyType } from './lib/types';
 export { getCurrency, getCurrencyLoadingStatus } from './model/currency-selectors';
+export { fetchCurrency } from './model/api-actions/fetch-currency';
