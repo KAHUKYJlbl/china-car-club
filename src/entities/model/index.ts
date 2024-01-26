9549aa38b51a482ead747c654d611c3c142cdca7
Iurii Pototskii <iuriipototskii@gmail.com>
2024-01-26 21:59:12 +0100

    feat: price options

--- src/entities/model/index.ts
+++ src/entities/model/index.ts
@@ -1,4 +1,4 @@
 export { modelSlice, setIdle, setPending } from './model/model-slice';
 export { fetchModel } from './model/api-actions/fetch-model';
 export { getSpecificationParams, getModelLoadingStatus, getModel } from './model/model-selectors';
-export type { SpecsType } from './lib/types';
+export type { SpecsType, PriceType } from './lib/types';
--- src/features/choose-options/ui/choose-options.tsx
+++ src/features/choose-options/ui/choose-options.tsx
@@ -1,20 +1,35 @@
 import { memo, useState } from 'react';
 import cn from 'classnames';
 
+import { AddsType } from '../../../widgets/model-info';
+import { getCurrency, getCurrencyLoadingStatus } from '../../../entities/currency';
+import { PriceType } from '../../../entities/model';
 import { Dropdown } from '../../../shared/ui/dropdown';
+import priceFormat from '../../../shared/lib/utils/price-format';
+import { useAppSelector } from '../../../shared/lib/hooks/use-app-selector';
 
 import classes from './choose-options.module.sass';
+import getTotal from '../lib/utils/getTotal';
+import { Currency } from '../lib/const';
+import { LoadingSpinner } from '../../../shared/ui/loading-spinner';
 
-enum Currency {
-  RUB = '₽',
-  USD = '$',
-  CNY = '¥',
-};
+type ChooseOptionsProps = {
+  prices: PriceType;
+  options: Record<AddsType, boolean>;
+  optionsHandler: (arg: AddsType) => void;
+}
 
 export const ChooseOptions = memo(
-  (): JSX.Element => {
+  ({ prices, options, optionsHandler }: ChooseOptionsProps): JSX.Element => {
+    const currency = useAppSelector(getCurrency);
+    const currencyLoadingStatus = useAppSelector(getCurrencyLoadingStatus);
     const [ currentCurrency, setCurrentCurrency ] = useState(Currency.RUB);
     const [ currentDelivery, setCurrentDelivery ] = useState<number | null>(null);
+
+    if (currencyLoadingStatus.isLoading || !currency) {
+      return <LoadingSpinner spinnerType='widget' />
+    }
+
 
     return (
       <div className={classes.wrapper}>
@@ -28,16 +43,49 @@ export const ChooseOptions = memo(
         <div className={classes.block}>
           <div className={classes.prices}>
             <span className={classes.price}>
-              0 000 000 {currentCurrency}
+              {
+                `${priceFormat(String(
+                  getTotal({
+                    totalPrice: prices.withLogistics,
+                    options,
+                    optionsPrices: {
+                      epts: 1000,
+                      guarantee: 1000,
+                      options: 0
+                    },
+                    currency,
+                    currentCurrency,
+                  })
+                ))} ${currentCurrency}`
+              }
             </span>
 
-            Цена на авто под ключ
+            <span className={classes.priceLabel}>
+              Цена на авто под ключ
+            </span>
 
             <span className={classes.price}>
-              0 000 000 {currentCurrency}
+            {
+                `${priceFormat(String(
+                  getTotal({
+                    totalPrice: prices.withLogistics,
+                    options,
+                    optionsPrices: {
+                      epts: 1000,
+                      guarantee: 1000,
+                      options: 0
+                    },
+                    currency,
+                    currentCurrency,
+                    discount: 5000
+                  })
+                ))} ${currentCurrency}`
+              }
             </span>
 
-            Цена со снижением
+            <span className={classes.priceLabel}>
+              Цена со снижением
+            </span>
 
             <div className={classes.buttons}>
               <button
@@ -68,11 +116,23 @@ export const ChooseOptions = memo(
               Выбрать доп.опции
             </div>
 
-            <div className={classes.optionsItem}>
+            <div
+              onClick={() => optionsHandler('epts')}
+              className={cn(
+                classes.optionsItem,
+                {[classes.current]: options.epts}
+              )}
+            >
               ЭПТС и утильсбор
             </div>
 
-            <div className={classes.optionsItem}>
+            <div
+              onClick={() => optionsHandler('guarantee')}
+              className={cn(
+                classes.optionsItem,
+                {[classes.current]: options.guarantee}
+              )}
+            >
               Гарантия на авто
             </div>
           </div>
