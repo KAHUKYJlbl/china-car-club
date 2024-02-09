0bf6c2f84f9ba52b8add9f556cfd2d1723e352ff
Iurii Pototskii <iuriipototskii@gmail.com>
2024-02-09 17:52:57 +0100

    fix: vercel deploy

--- src/widgets/calculator/lib/hooks/use-filters.ts
+++ src/widgets/calculator/lib/hooks/use-filters.ts
@@ -5,7 +5,7 @@ import { FilterId } from '../../../../features/filter';
 import { fetchFiltered } from '../../../../entities/manufacturer';
 import { useAppDispatch } from '../../../../shared/lib/hooks/use-app-dispatch';
 
-const useFilters = (filters: Partial<Record<FilterId, number[]>>, isPromo: boolean) => {
+const useFilters = (filters: Partial<Record<FilterId, number[]>>) => {
   const dispatch = useAppDispatch();
   const [ filtersToFetch ] = useDebounce(filters, 650);
 
