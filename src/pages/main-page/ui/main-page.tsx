79547500c618e9fdd7d8b1bbaac7683088ba456c
Iurii Pototskii <iuriipototskii@gmail.com>
2024-02-09 12:33:27 +0100

    feat: robots.txt

--- src/pages/main-page/ui/main-page.tsx
+++ src/pages/main-page/ui/main-page.tsx
@@ -1,7 +1,14 @@
+import { useEffect } from 'react';
 import { Navigate } from 'react-router-dom';
 
 import { AppRoute } from '../../../app/provider/router';
 
-const MainPage = (): JSX.Element => <Navigate to={AppRoute.Calc} />;
+const MainPage = (): JSX.Element => {
+  useEffect(() => {
+    window.location.replace('https://chinacar.club');
+  }, []);
+
+  return <Navigate to={AppRoute.Calc} />;
+}
 
 export default MainPage;
