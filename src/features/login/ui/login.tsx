a5ccba53a8844d3fb77f6b63aec51f015baf8339
Iurii Pototskii <iuriipototskii@gmail.com>
2024-02-22 14:54:07 +0100

    fix: telegram auth

--- src/features/login/ui/login.tsx
+++ src/features/login/ui/login.tsx
@@ -10,8 +10,6 @@ type LoginProps = {
 }
 
 export const Login = ({onClose, onLoginSuccess}: LoginProps): JSX.Element => {
-  console.log(onLoginSuccess);
-
   const smsHandler = () => {
     toast('Сервис SMS недоступен.', {type: 'warning'});
     toast('Попробуйте авторизацию через Telegram.', {type: 'warning'});
@@ -44,7 +42,7 @@ export const Login = ({onClose, onLoginSuccess}: LoginProps): JSX.Element => {
           <TelegramLoginButton
             botName="test111_login_bot"
             cornerRadius={12}
-            dataOnauth={(user: TelegramUser) => console.log(user)}
+            dataOnauth={onLoginSuccess}
           />
         </div>
       </div>
