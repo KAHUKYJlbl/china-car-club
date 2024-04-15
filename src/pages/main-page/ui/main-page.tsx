4b451e3c77e69e099fb80c19b25be865bf9528c6
Iurii Pototskii <iuriipototskii@gmail.com>
2024-04-15 15:23:52 +0200

    fix: fixes

--- src/pages/main-page/ui/main-page.tsx
+++ src/pages/main-page/ui/main-page.tsx
@@ -1,14 +1,7 @@
-import { useEffect } from 'react';
 import { Navigate } from 'react-router-dom';
 
 import { AppRoute } from '../../../app/provider/router';
 
-const MainPage = (): JSX.Element => {
-  useEffect(() => {
-    window.location.replace('https://chinacar.club');
-  }, []);
-
-  return <Navigate to={AppRoute.Calc} />;
-}
+const MainPage = (): JSX.Element => <Navigate to={AppRoute.Calc} />;
 
 export default MainPage;
