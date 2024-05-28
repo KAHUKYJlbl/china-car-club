b60fb819f332d78cc72e21a28ae9d00e9f632f2f
Iurii Pototskii <iuriipototskii@gmail.com>
2024-05-28 23:03:18 +0200

    docs: release version

--- src/entities/user/index.ts
+++ src/entities/user/index.ts
@@ -9,6 +9,6 @@ export { userSlice, setCity, setAutoLocation, login } from "./model/user-slice";
 export { fetchHash } from "./model/api-actons/fetch-hash";
 export { postStatistics } from "./model/api-actons/post-statistics";
 export { postRefresh } from "./model/api-actons/post-refresh";
-export type { SmsFormType, ConfirmType, LocationType } from "./lib/types";
 export { Login } from "./ui/login";
 export { HeaderUser } from "./ui/header-user";
+export type { SmsFormType, ConfirmType, LocationType } from "./lib/types";
