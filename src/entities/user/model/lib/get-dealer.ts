0b2065b33c142f415fbf3b41bada7e6e4c577372
Iurii Pototskii <iuriipototskii@gmail.com>
2024-08-09 13:39:34 +0200

    fix: get dealer

--- src/entities/user/model/lib/get-dealer.ts
+++ src/entities/user/model/lib/get-dealer.ts
@@ -3,7 +3,7 @@ export const getDealerLogin = () => {
   switch (window.location.origin) {
     case "https://import2.lixiangrussia.ru":
       return "E.N.CARS";
-    case "https://import2.lixiangrussia.ru":
+    case "https://import.lixiangrussia.ru":
       return "Рольф Импорт";
     default:
       return "Chinacar.club";
--- src/widgets/model-info/lib/utils/get-dealer.ts
+++ src/widgets/model-info/lib/utils/get-dealer.ts
@@ -2,7 +2,7 @@ export const getDealer = () => {
   switch (window.location.origin) {
     case "https://import2.lixiangrussia.ru":
       return "E.N.CARS";
-    case "https://import2.lixiangrussia.ru":
+    case "https://import.lixiangrussia.ru":
       return "Рольф";
     default:
       return "Chinacar.club";
