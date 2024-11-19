3c0702ab14810962ac8d763df9390c21bafd37c1
Iurii Pototskii <iuriipototskii@gmail.com>
2024-11-19 01:15:14 +0100

    fix: filters

--- src/features/filter/lib/hooks/use-active-filters.ts
+++ src/features/filter/lib/hooks/use-active-filters.ts
@@ -1,16 +1,17 @@
-import { FilterId } from '../types';
+import { FilterId } from "../types";
 
-const useActiveFilters = (activeFilters: Partial< Record< FilterId, number[] > >) => {
+const useActiveFilters = (activeFilters: Partial<Record<FilterId, number[]>>) => {
   const filtersArray = Object.values(activeFilters);
+
   let isActiveFilters = false;
 
-  if ( filtersArray.length !== 0 ) {
-    filtersArray.forEach(( filters ) => {
+  if (filtersArray.length !== 0) {
+    filtersArray.forEach((filters) => {
       if (filters.length !== 0) {
         isActiveFilters = true;
       }
     });
-  };
+  }
 
   return isActiveFilters;
 };
