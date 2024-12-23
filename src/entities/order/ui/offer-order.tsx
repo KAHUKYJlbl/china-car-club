68081fade94cae08d76baef0035d375af823f3a5
Iurii Pototskii <iuriipototskii@gmail.com>
2024-12-23 11:00:37 +0100

    fix: colors

--- src/entities/order/ui/offer-order.tsx
+++ src/entities/order/ui/offer-order.tsx
@@ -11,97 +11,98 @@ export const OfferOrder = ({ offer }: OfferOrderProps) => {
       <div className={classes.wrapper}>
         <p className={classes.header}>Основные характеристики</p>
 
-        {offer.specification.parameters.bodyType.name && (
+        {!!offer.specification.parameters.bodyType.name && (
           <div className={classes.parameter}>
             <p>Кузов</p>
             <p>{offer.specification.parameters.bodyType.name}</p>
           </div>
         )}
 
-        {offer.specification.parameters.transmissionType.name && (
+        {!!offer.specification.parameters.transmissionType.name && (
           <div className={classes.parameter}>
             <p>Коробка</p>
             <p>{offer.specification.parameters.transmissionType.name}</p>
           </div>
         )}
 
-        {offer.specification.parameters.power && (
+        {!!offer.specification.parameters.power && (
           <div className={classes.parameter}>
             <p>Мощность двигателя</p>
             <p>{offer.specification.parameters.power} кВт ⋅ ч</p>
           </div>
         )}
 
-        {offer.specification.parameters.torque && (
+        {!!offer.specification.parameters.torque && (
           <div className={classes.parameter}>
             <p>Крутящий момент</p>
             <p>{offer.specification.parameters.torque} Н ⋅ м</p>
           </div>
         )}
 
-        {offer.specification.parameters.batteryCapacity && (
+        {!!offer.specification.parameters.batteryCapacity && (
           <div className={classes.parameter}>
             <p>Батарея</p>
             <p>{offer.specification.parameters.batteryCapacity}</p>
           </div>
         )}
-        {offer.specification.parameters.powerReserve && (
+
+        {!!offer.specification.parameters.powerReserve && (
           <div className={classes.parameter}>
             <p>Запас хода на топливе</p>
             <p>{offer.specification.parameters.powerReserve} км</p>
           </div>
         )}
 
-        {offer.specification.parameters.electricPowerReserve && (
+        {!!offer.specification.parameters.electricPowerReserve && (
           <div className={classes.parameter}>
             <p>Запас хода на батарее</p>
             <p>{offer.specification.parameters.electricPowerReserve} км</p>
           </div>
         )}
 
-        {offer.specification.parameters.engineCount && (
+        {!!offer.specification.parameters.engineCount && (
           <div className={classes.parameter}>
             <p>Количество двигателей</p>
             <p>{offer.specification.parameters.engineCount}</p>
           </div>
         )}
 
-        {offer.specification.parameters.seats && (
+        {!!offer.specification.parameters.seats && (
           <div className={classes.parameter}>
             <p>Количество мест</p>
             <p>{offer.specification.parameters.seats}</p>
           </div>
         )}
 
-        {offer.specification.parameters.lengthWidthHeight && (
+        {!!offer.specification.parameters.lengthWidthHeight && (
           <div className={classes.parameter}>
             <p>Длина * Ширина * Высота</p>
             <p>{offer.specification.parameters.lengthWidthHeight} мм</p>
           </div>
         )}
 
-        {offer.specification.parameters.groundClearance && (
+        {!!offer.specification.parameters.groundClearance && (
           <div className={classes.parameter}>
             <p>Клиренс</p>
             <p>{offer.specification.parameters.groundClearance}</p>
           </div>
         )}
 
-        {offer.specification.parameters.wheelSize.front && (
+        {!!offer.specification.parameters.wheelSize.front && (
           <div className={classes.parameter}>
             <p>Передние колеса</p>
             <p>{offer.specification.parameters.wheelSize.front}</p>
           </div>
         )}
 
-        {offer.specification.parameters.wheelSize.rear && (
+        {!!offer.specification.parameters.wheelSize.rear && (
           <div className={classes.parameter}>
             <p>Задние колеса</p>
             <p>{offer.specification.parameters.wheelSize.rear}</p>
           </div>
         )}
 
-        {offer.specification.parameters.engineCapacity && (
+        {!!offer.specification.parameters.engineCapacity && (
           <div className={classes.parameter}>
             <p>Объем двигателя</p>
             <p>{offer.specification.parameters.engineCapacity} см. куб</p>
@@ -109,7 +110,7 @@ export const OfferOrder = ({ offer }: OfferOrderProps) => {
         )}
       </div>
 
-      {false && (
+      {!!offer.colors[0] && !!offer.colors[0].items[0] && !!offer.colors[1] && !!offer.colors[1].items[0] && (
         <div className={classes.wrapper}>
           <p className={classes.header}>Цвет кузова и салона</p>
 
